340f35abead1ec3416fba2eb574016bc33d1a03d
harshithb3304 <harsh03032004@gmail.com>
2024-08-24 18:41:02 +0530

    About us in Home

--- src/app/_components/LandingPage.jsx
+++ src/app/_components/LandingPage.jsx
@@ -10,7 +10,7 @@ const LandingPage = () => {
           About Us
         </p>
         <p className="w-1/2 mt-10">
-          Lorem Ipsum is simply dummy text of the printing and  typesetting industry. Lorem Ipsum has been the industry's standard dummy  text ever since the 1500s, when an unknown printer took a galley of  type and scrambled it to make a type specimen book.
+          Lorem Ipsum is simply dummy text of the printing and  typesetting industry. Lorem Ipsum has been the industrys standard dummy  text ever since the 1500s, when an unknown printer took a galley of  type and scrambled it to make a type specimen book.
         </p>
       </div>
     </main>
