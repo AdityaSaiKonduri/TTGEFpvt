b4725a4383354d9b9cb83d61f032fb2d0b1c363e
harshithb3304 <harsh03032004@gmail.com>
2024-08-24 18:39:09 +0530

    About us in Home

--- src/app/_components/LandingPage.jsx
+++ src/app/_components/LandingPage.jsx
@@ -3,8 +3,16 @@ import Navbar from './navbar'
 
 const LandingPage = () => {
   return (
-    <main className = "h-screen bg_custom_gradient">
-      <Navbar/>
+    <main className="h-screen bg_custom_gradient">
+      <Navbar />
+      <div className="flex flex-col justify-center items-center mt-32">
+        <p className="text-5xl">
+          About Us
+        </p>
+        <p className="w-1/2 mt-10">
+          Lorem Ipsum is simply dummy text of the printing and  typesetting industry. Lorem Ipsum has been the industry's standard dummy  text ever since the 1500s, when an unknown printer took a galley of  type and scrambled it to make a type specimen book.
+        </p>
+      </div>
     </main>
   )
 }
