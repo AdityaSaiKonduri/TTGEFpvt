39f6d1f2621b33d7eab89e9f42093972d99f2d1b
AdityaSaiKonduri <adityakonduri8@gmail.com>
2024-08-24 14:14:56 +0530

    Landing Page and scroll to top

---
+++ src/app/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import LandingImage from './_components/LandingImage'
+import Dummy from './_components/Dummy'
+
+const page = () => {
+  return (
+    <main>
+      <LandingImage />
+      <Dummy />
+    </main>
+  )
+}
+
+export default page
